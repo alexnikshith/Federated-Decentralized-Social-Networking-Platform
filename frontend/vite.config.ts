c8a8e4b453630e6e6a526fdd2e8913853572100a
Riteesh T M <riteesh.meganathan@gmail.com>
2026-03-03 02:23:13 +0530

    Added allowed host for server to render ui

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig(({ mode }) => ({
     hmr: {
       overlay: false,
     },
+    allowedHosts: ["federated-decentralized-social-lwlb.onrender.com"],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
